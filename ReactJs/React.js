e9f99247358b99c68872b4c9cb30a9159a27d5dd
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-02-26 15:45:45 +0000

    React OverView

---
+++ ReactJs/React.js
@@ -0,0 +1,10 @@
+//ReactJs:- is a js library using it u can build userinterface by the help of components like header, footer, navbar etc...
+// it works on single page application (SPA)
+//SPA: the application where all content will be on single page and the url will update to gate in different section but webpage will not refresh
+//multi page application: the entire webpage and url will change 
+//one page website: all content will be on single home page 
+
+/* REACT(Developed and maintained by facebook)
+1) it works on Virtual DOM 
+
+*/
