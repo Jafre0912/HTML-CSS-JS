30f81fdb8d911126009b658855965758fd356cdd
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-02-26 16:40:10 +0000

    Overview

---
+++ ReactJs/Calculator.js
@@ -0,0 +1,7 @@
+let subData=(x,y)=>x-y;
+export default subData;
+
+let mulData=(a,b)=>(a*b);
+let divData=(a,b)=>(a/b);
+export let name = "jafar";
+export {mulData,divData};
--- ReactJs/React.js
+++ ReactJs/React.js
@@ -1,3 +1,5 @@
+// ECMAScript (ES) is the official name for JavaScript.
+
 //ReactJs:- is a js library using it u can build userinterface by the help of components like header, footer, navbar etc...
 // it works on single page application (SPA)
 //SPA: the application where all content will be on single page and the url will update to gate in different section but webpage will not refresh
@@ -7,4 +9,27 @@
 /* REACT(Developed and maintained by facebook)
 1) it works on Virtual DOM 
 
+2)Viratula DOM: it is the cope of real DOM... whenever react makes any change/update in componanets that only react does not reload the full website it only update that part only that has been changed in the component... bby the help of virtual DOM
+3) and jo v change hota hh uske vDom real dom me update krr deta hh 
+
+4) realDOM: this is real stracture of browser 
+
+
+
+6) npm init -y : creates package.json file in our folder 
+
+6) package.json : where we show which file need to run as a main file 
+7)JSX: js+xml
+
+8) nodemon: this command automatic change our file data whenever we try to change or update it
 */
+
+//5) DEFAULT EXPORT : default export allows you to export a single value like function only, class only, object only, or variable) 
+    //eg: 
+    let sumData=(a,b)=>a+b;
+    console.log(sumData(2,3));
+    export default sumData;
+//   Named export: allows to export multiple
+let subdata=(x,y)=>(x-y);
+console.log(subdata(5,4));
+export{subdata}; 
