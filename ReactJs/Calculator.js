77c724aa503622ffb844918e7d9801c79caac14e
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-03-06 16:16:20 +0000

    overview

--- ReactJs/Calculator.js
+++ ReactJs/Calculator.js
@@ -3,5 +3,6 @@ export default subData;
 
 let mulData=(a,b)=>(a*b);
 let divData=(a,b)=>(a/b);
+let DivD=(x,y)=>(x/y);
 export let name = "jafar";
-export {mulData,divData};
+export {mulData,divData,DivD};
--- ReactJs/index1.js
+++ ReactJs/index1.js
@@ -1,7 +1,7 @@
 import subData  from "./Calculator.js";
 console.log(subData(9,3));
-
-import { mulData as mul,divData,name } from "./Calculator.js"; //importing named export fxn using curly braces
-console.log(mul(2,2)); // here mul is used as mulData fxn this is called aliyas
+import { mulData as mul,divData,name,DivD} from "./Calculator.js"; //importing named export fxn using curly brace
+console.log(mul(2,2)); // here mul is used as mulData fxn this is called aliyas 
 console.log(name);
+console.log(DivD);
 console.log(divData(4,2));
