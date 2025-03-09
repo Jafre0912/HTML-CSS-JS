5c16065fef5806d1ed3cd99b25557ac6c2490bd7
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-03-09 08:53:40 +0000

    js

--- JS/Theory.js
+++ JS/Theory.js
@@ -1,1 +1,8 @@
 //event loop : Event loop continuously check karta hai ki async code complete hua ya nahi. Agar complete ho jaye, to wo async code Call Stack me move hota hai Task Queue ya Microtask Queue se. Ye process event loop tab tak check karta rehta hai jab tak saare async operations complete na ho jaye.
+console.log("hey"); //sync
+console.log("alam");
+setTimeout(() => {  //will stay in queue
+    console.log("hey")
+}, 0);
+
+console.log("how are u");
