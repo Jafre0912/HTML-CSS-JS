49871854dbcc6bd129fa6bb8d5635a951936256a
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-03-14 04:26:48 +0000

    ArrowFxn

---
+++ JS/Arrowfxn.js
@@ -0,0 +1,33 @@
+//arrow function
+let addData=()=>console.log("hy jafar");
+addData();
+
+
+//returning arrow fxn wothout return keyword if {} braces not uses the directly =>num1*num2
+let addData1 = (num1,num2)=>num1*num2; //here not required to write the return num1*num2 and return =>num1*num2
+console.log(addData1(10,2)); 
+
+
+let addData2 = num1 =>num1; //for single parameter no need to use(num1)
+let ans = addData2(5);
+console.log(ans);
+
+
+let addData3 = (a,b)=>{ //if {} braces use then reutrn is mandatory to use otherwise o/p will not get
+    return a+b;
+}
+let ans1 = addData3(2,4)
+console.log(ans1)
+
+//using default value
+let myfun = (n1=10,n2=5)=>{
+    return{
+        sum:n1+n2,
+        diff:n1-n2,
+        mul:n1*n2,
+        div:n1/n2
+    }
+}
+//let myAns = myfun(10,10) (if u will not pass any argument then default value n1,n2=5 will assigned )
+let myDefaultAns = myfun()
+console.log(myDefaultAns.sum);
