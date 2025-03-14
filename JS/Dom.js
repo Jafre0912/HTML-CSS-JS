5f2e3069aab5b6a71cfbe3b62ac12bef5d7948e9
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-03-14 06:36:05 +0000

    DOM

---
+++ JS/Dom.js
@@ -0,0 +1,5 @@
+//DOM: the tree stracture of a webpage like html, head, title, p, heading, th, tr, div, main, footer, navbar, header etc...
+// by using this we can modify the particular part of webpage stracutre 
+
+
+//document.getelementbyId (this provide the facility to target any html element)
