681a8fa6adc3f948b4c4b5e7490f9de8a48bf359
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-04-03 17:41:50 +0000

    commit

--- ReactJs/React.js
+++ ReactJs/React.js
@@ -3,7 +3,7 @@
 //ReactJs:- is a javascripts library using it u can build userinterface by the help of components like header, footer, navbar etc...
 // it works on single page application (SPA)
 //SPA: the application where all content will be on single page and the url will update to gate in different section but webpage will not refresh
-//multi page application: the entire webpage and url will change 
+//multi page applications: the entire webpage and url will change 
 //one page website: all content will be on single home page 
 
 /* REACT(Developed and maintained by facebook)
