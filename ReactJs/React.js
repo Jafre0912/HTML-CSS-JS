238238fd2d7eae9e6a5804a5b7aba8afb1a190d5
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-04-03 17:42:54 +0000

    ommit

--- ReactJs/React.js
+++ ReactJs/React.js
@@ -4,7 +4,7 @@
 // it works on single page application (SPA)
 //SPA: the application where all content will be on single page and the url will update to gate in different section but webpage will not refresh
 //multi page applications: the entire webpages and url will change 
-//one page website: all content will be on single home page 
+//one page websites: all content will be on single home page 
 
 /* REACT(Developed and maintained by facebook)
 1) it works on Virtual DOM 
