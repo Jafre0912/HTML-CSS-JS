4405874bcc3e89e6c1ecde62e8926aa3ed3406bf
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-05-23 13:08:39 +0000

    theory

---
+++ ReactJs/ReactTheory.js
@@ -0,0 +1,5 @@
+/*
+1) SPA: Single Page Application: in which all the data/pages will be there on the sigle page only but the urk will change 
+2) 1page Application: where page will load on the same page only but the url will not change
+3)multi page application: whole url and page will load 
+*/
